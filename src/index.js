5097b50bd9dcc151a0ac5e1335d5aa6dd3cb95b0
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-04-21 18:20:07 +0300

    Implement nested routing using Outlet and useOutletContext

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes.js";
-import "./index.css"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
